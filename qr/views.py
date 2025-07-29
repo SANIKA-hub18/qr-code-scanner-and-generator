bd4ade94c7907ade15d8ab525bcdd19695d5ad78
Sanika Shaligram <shaligramsanika@gmail.com>
2025-07-29 23:05:24 +0530

    Removed pyzbar and switched to OpenCV QR detection

--- qr/views.py
+++ qr/views.py
@@ -4,24 +4,23 @@ import qrcode.image.svg
 from io import BytesIO
 import base64
 import numpy as np
-from pyzbar.pyzbar import decode
+import cv2  # ✅ Using OpenCV instead of pyzbar
 from PIL import Image
 from django.http import HttpResponse
 
 
 class QrCodeView(TemplateView):
-    template_name='qr.html'
+    template_name = 'qr.html'
 
     def get_context_data(self, **kwargs):
         context = super(QrCodeView, self).get_context_data(**kwargs)
-        # fill your data here
-        qrcode =  self.get_qrcode_svg('{}&{}'.format('This is the qrcode data guys.','bip-zip'))
-        context.update({"qrcode":qrcode})
+        qrcode_img = self.get_qrcode_svg('{}&{}'.format('This is the qrcode data guys.', 'bip-zip'))
+        context.update({"qrcode": qrcode_img})
         return context
-    
+
     def get_qrcode_svg(self, text):
         factory = qrcode.image.svg.SvgImage
-        img = qrcode.make(text ,image_factory=factory, box_size=30)
+        img = qrcode.make(text, image_factory=factory, box_size=30)
         stream = BytesIO()
         img.save(stream)
         base64_image = base64.b64encode(stream.getvalue()).decode()
@@ -29,38 +28,29 @@ class QrCodeView(TemplateView):
 
 
 class QrCodeScan(TemplateView):
-    template_name= 'qrscanner.html'
+    template_name = 'qrscanner.html'
 
-    def post(self,request):
+    def post(self, request):
         image = request.POST['image']
         image_data = base64.b64decode(image.split(',')[1])
 
         img = BytesIO(image_data)
         data = self.qrcodeReader(img)
-        if data == False :
-            # your error message
+        if data is False:
             return HttpResponse('No Qr code found! Sorry.')
         return HttpResponse(data)
-        # send data whereever you want :
-        # return redirect("/add/?qrdata={}".format(data))
-    
+
     def qrcodeReader(self, img):
-        # Read the image data using Pillow
-        image = Image.open(img)
-        # Convert the image to grayscale
-        gray_image = image.convert('L')
-        # Convert the grayscale image to a NumPy array
-        np_image = np.array(gray_image)
-        # Decode the QR code in the image using pyzbar
-        decoded = decode(np_image)
-        # Get the data from the QR code
-        if len(decoded) > 0:
-            data = decoded[0].data.decode('utf-8')
-        else:
-            data = False
-        return data
+        # Read the uploaded image with Pillow
+        image = Image.open(img).convert('RGB')
+        # Convert to NumPy array (OpenCV format)
+        np_image = np.array(image)
+        # Convert RGB to BGR (as OpenCV uses BGR format)
+        bgr_image = cv2.cvtColor(np_image, cv2.COLOR_RGB2BGR)
         
-    
-
-
-
+        detector = cv2.QRCodeDetector()
+        data, bbox, _ = detector.detectAndDecode(bgr_image)
+        if data:
+            return data
+        else:
+            return False
